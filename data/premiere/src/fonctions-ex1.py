857a44a55bd984cfd716c27b399ea94c5ab97e40
Marie Holzer <137159221+holzermarie@users.noreply.github.com>
2024-11-18 11:31:42 +0100

    [1nsi] Ajout fonctions

---
+++ data/premiere/src/fonctions-ex1.py
@@ -0,0 +1,15 @@
+# FONCTIONS
+def addition(a, b):
+    somme = a + b
+    return somme
+
+def multiplication(a, b):
+    produit = a * b
+    return produit
+
+# SCRIPT
+a = 5
+p = multiplication(a + 3, 7)
+s = 3
+m = multiplication(s, 6)
+print(a, p, s, m)
