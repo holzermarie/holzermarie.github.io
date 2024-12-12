3fe48f66ed441ec1dfe9161d0ee28259ccf6560b
Marie Holzer <137159221+holzermarie@users.noreply.github.com>
2024-12-12 15:50:47 +0100

    [tnsi] BDD : create tables de l'exo 5

---
+++ data/term/src/ex5_create_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE "Client" (
+	"idC"	INTEGER,
+	"nom"	TEXT,
+	"prenom"	TEXT,
+	"email"	TEXT,
+	PRIMARY KEY("idC" AUTOINCREMENT)
+)
+
+CREATE TABLE "Location" (
+	"idL"	INTEGER,
+	"idCli"	INTEGER,
+	"idVehi"	INTEGER,
+	"dateDeb"	TEXT,
+	"dateFin"	TEXT,
+	FOREIGN KEY("idVehi") REFERENCES "Vehicule"("idV"),
+	FOREIGN KEY("idCli") REFERENCES "Client"("idC"),
+	PRIMARY KEY("idL" AUTOINCREMENT)
+)
+
+CREATE TABLE "Modele" (
+	"idM"	INTEGER,
+	"marque"	TEXT,
+	"annee"	INTEGER,
+	"moteur"	TEXT,
+	PRIMARY KEY("idM" AUTOINCREMENT)
+)
+
+CREATE TABLE "Vehicule" (
+	"idV"	INTEGER,
+	"modele"	TEXT,
+	"immatr"	TEXT,
+	"nbKm"	INTEGER,
+	FOREIGN KEY("modele") REFERENCES "Modele"("idM"),
+	PRIMARY KEY("idV" AUTOINCREMENT)
+)
