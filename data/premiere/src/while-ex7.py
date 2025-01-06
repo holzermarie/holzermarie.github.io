513e2ae1c3041c8ddffc74e1b03396fa2843feef
Marie Holzer <137159221+holzermarie@users.noreply.github.com>
2025-01-06 11:03:37 +0100

    [1nsi] fix ex 7 while

--- data/premiere/src/while-ex7.py
+++ data/premiere/src/while-ex7.py
@@ -3,7 +3,7 @@
 def somme_inverses(n):
     somme = 0
     denominateur = 1
-    while denominateur < n:
+    while denominateur <= n:
         somme = somme + 1/denominateur
         denominateur = denominateur + 1
     return somme
@@ -11,9 +11,9 @@ def somme_inverses(n):
     
 # SCRIPT
 # tests
-assert somme_inverses(1) == 0
-assert somme_inverses(2) == 1
-assert somme_inverses(4) == 0 + 1 + 1/2 + 1/3
+assert somme_inverses(1) == 1
+assert somme_inverses(2) == 1 + 1/2
+assert somme_inverses(4) == 1 + 1/2 + 1/3 + 1/4
 
 # 2) plus petit entier n tel que Hn > 10
 n = 0
