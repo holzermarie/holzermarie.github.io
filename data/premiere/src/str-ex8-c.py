34bf035b859e4ddb843d6b7ead9d4547d2d9335e
Marie Holzer <137159221+holzermarie@users.noreply.github.com>
2025-01-20 11:42:59 +0100

    [1nsi] fichier test encodage pour strings

--- data/premiere/src/str-ex8-c.py
+++ data/premiere/src/str-ex8-c.py
@@ -1,26 +1,28 @@
 # FONCTIONS
 def est_numerique(car):
-    return ord(car) >= 48 and ord(car) <=57
+    return ord(car) >= 48 and ord(car) <= 57
 
-def est_une_date_valide(chaine):
+def est_une_date_valide(chaine): # chaine de format jj/mm/aaaa
     if len(chaine) != 10:
         return False
     if chaine[2] != "/" or chaine[5] != "/":
         return False
+    
     idx = 0
-    while idx < 10:
+    while idx < 10: # parcourir la chaîne
         if idx != 2 and idx != 5:
             if not est_numerique(chaine[idx]):
                 return False
-            jour = chaine[0] + chaine[1]
-            # teste si jour ok
-            if int(jour) < 1 or int(jour) > 31:
-                return False
-            # teste si mois ok
-            mois = chaine[3] + chaine[4]
-            if int(mois) < 1 or int(mois) > 12:
-                return False
         idx += 1
+        
+    jour = chaine[0] + chaine[1]
+    # teste si jour ok
+    if int(jour) < 1 or int(jour) > 31:
+        return False
+    # teste si mois ok
+    mois = chaine[3] + chaine[4]
+    if int(mois) < 1 or int(mois) > 12:
+        return False
     return True
 
 # SCRIPT
