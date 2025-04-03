6f5772959488d0280435bdf3a8688d36b7ec2fe6
Marie Holzer <137159221+holzermarie@users.noreply.github.com>
2025-04-03 17:56:56 +0200

    [1nsi] corrigés dictionnaires avec TD en plus en Python

--- data/premiere/src/dictionnaires_td-c.py
+++ data/premiere/src/dictionnaires_td-c.py
@@ -5,6 +5,13 @@ def est_present(tab, v):
             return True 
     return False
 
+def trouve_maximum(t):
+    maxi = 0
+    for val in t:
+        if val > maxi:
+            maxi = val
+    return maxi
+
 def depouillement(candidats, votes):
     occurences = {cle:0 for cle in candidats}
     occurences["Blanc"] = 0
@@ -18,12 +25,36 @@ def depouillement(candidats, votes):
             occurences["Nul"] += 1
     return occurences
     
+def vainqueur(d):
+    nb_vote_max = 0
+    elu = ""
+    for candidat, nb_voies in d.items():
+        if nb_voies > nb_vote_max:
+            nb_vote_max = nb_voies
+            elu = candidat
+    return elu
 
 # SCRIPT
+candidats = ["Alan Turing", "Ada Lovelace", "George Boole"]
+occurrences = {"Nul": 0, "Blanc": 0}
+for candidat in candidats:
+    occurrences[candidat] = 0
+print(occurrences)
+
 tab = [3, 5, 7, 9, 1]
 print(est_present(tab, 5))
+assert est_present(tab, 5)
 print(est_present(tab, 22))
+assert not est_present(tab, 22)
+
+assert trouve_maximum(tab) == 9
 
 candidats = ["Alan Turing", "Ada Lovelace", "George Boole"]
 votes = ["Alan Turing", "Ada Lovelace", "Ada Lovelace", "", "George Boole", "Mark Zukerberg", ""]
-print(depouillement(candidats, votes))
+res_election = depouillement(candidats, votes)
+assert res_election == {'Alan Turing': 1, 'Ada Lovelace': 2, 'George Boole': 1, 'Blanc': 2, 'Nul': 1}
+print(res_election) # doit afficher {'Alan Turing': 1, 'Ada Lovelace': 2, 'George Boole': 1, 'Blanc': 2, 'Nul': 1}
+
+resultat = vainqueur(res_election)
+print(resultat) # doit afficher "Ada Lovelace"
+assert resultat == "Ada Lovelace"
