6d526d8ced8dbf2f23720f60a076f2fe1d5182ae
Marie Holzer <137159221+holzermarie@users.noreply.github.com>
2026-02-20 10:44:48 +0100

    [1nsi] DM tableaux, template

---
+++ data/premiere/src/tableaux-dm.py
@@ -0,0 +1,69 @@
+# IMPORTS
+# mettez vos éventuels imports ici...
+
+#####################################################################
+# FONCTIONS
+
+############
+# Exercice 2
+###
+# Question 1
+# votre fonction ici...
+
+###
+# Question 2
+# votre fonction ici...
+
+############
+# Exercice 3
+###
+# Question 1
+# votre fonction ici...
+
+###
+# Question 2
+# votre fonction ici...
+
+############
+# Exercice 7
+###
+# Question 1
+# votre fonction ici...
+
+# ne pas faire la question 2 !!!
+
+############
+# Exercice 15
+# votre fonction ici...
+
+############
+# Exercice 17
+# votre fonction ici...
+
+#####################################################################
+# SCRIPT
+# Des affichages sont prévus pour bien distinguer les résultats dans la console. Ne pas les enlever !
+
+print("----Exercice 2----")
+print("1)")
+# votre code ici ...
+print("2)")
+# votre code ici ...
+
+print("----Exercice 3----")
+print("1)")
+# votre code ici ...
+print("2)")
+# votre code ici ...
+
+print("----Exercice 7----")
+print("1)")
+# votre code ici ...
+
+# ne pas faire la question 2 !!!
+
+print("----Exercice 15----")
+# votre code ici ...
+
+print("----Exercice 17----")
+# votre code ici ...
